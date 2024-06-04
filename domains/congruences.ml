cc5e23fcfb71807a166c4fca58a52f515466210a
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2024-06-04 18:37:03 +0200

    Reverted dune format, back to my beautiful hand-made formatting

--- domains/congruences.ml
+++ domains/congruences.ml
@@ -1,148 +1,138 @@
 open Frontend
 open Abstract_syntax_tree
 
-module CongruencesDomain = struct
-  (* type of abstract elements *)
-  (* an element of type t abstracts a set of integers *)
-  (* an abstract element is either the empty set (Bot) or the set of
-   * the elements of the form a*n+b for n in Z
-   * note that Top is represented by 1*n+0 for n in Z
-   *)
-  type t = Bot | Modulo of Z.t * Z.t
+module CongruencesDomain =
+  struct
 
-  (* unrestricted value: ℤ *)
-  let top = Modulo (Z.one, Z.zero)
+    (* type of abstract elements *)
+    (* an element of type t abstracts a set of integers *)
+    (* an abstract element is either the empty set (Bot) or the set of
+     * the elements of the form a*n+b for n in Z
+     * note that Top is represented by 1*n+0 for n in Z
+    *)
+    type t = | Bot | Modulo of Z.t * Z.t
 
-  (* bottom value: empty set *)
-  let bottom = Bot
+    (* unrestricted value: ℤ *)
+    let top = Modulo (Z.one, Z.zero)
 
-  (* constant: {c} *)
-  let const c = Modulo (Z.zero, c)
+    (* bottom value: empty set *)
+    let bottom = Bot
 
-  (* interval: [a,b] *)
-  let rand a b = if a = b then const a else top
+    (* constant: {c} *)
+    let const c = Modulo (Z.zero, c)
 
-  (* unary operation *)
-  let unary x = function
+    (* interval: [a,b] *)
+    let rand a b = if a = b then const a else top
+
+    (* unary operation *)
+    let unary x = function
     | AST_UNARY_PLUS -> x
-    | AST_UNARY_MINUS -> (
-        match x with Bot -> Bot | Modulo (a, b) -> Modulo (a, Z.neg b))
+    | AST_UNARY_MINUS -> match x with
+                         | Bot -> Bot
+                         | Modulo (a, b) -> Modulo(a, Z.neg b)
 
-  (* binary operation *)
-  let binary x y op =
-    match (x, y) with
-    | Bot, _ | _, Bot -> Bot
-    | Modulo (a1, b1), Modulo (a2, b2) -> (
+    (* binary operation *)
+    let binary x y op = 
+      match x, y with
+      | Bot, _ | _, Bot -> Bot
+      | Modulo(a1, b1), Modulo(a2, b2) -> match op with
+                                          | AST_PLUS -> Modulo(Z.gcd a1 a2, Z.add b1 b2)
+                                          | AST_MINUS -> Modulo(Z.gcd a1 a2, Z.sub b1 b2)
+                                          | AST_MULTIPLY -> Modulo(Z.gcd (Z.mul a1 a2) (Z.gcd (Z.mul a2 b1) (Z.mul a1 b2)) , Z.mul b1 b2)
+                                          | AST_DIVIDE when a2 = Z.zero -> 
+                                            if b2 = Z.zero then Bot (* division by zero *)
+                                            else if a1 = Z.zero then Modulo(Z.zero, Z.div b1 b2) (* constant divided by a constant *)
+                                            else if b2 <= Z.gcd a1 b1 then Modulo(Z.div a1 b2, Z.div a1 b2)
+                                            else top
+                                          | AST_DIVIDE -> top
+                                          | AST_MODULO when (a2, b2) = (Z.zero, Z.zero) -> Bot (* division by zero *)
+                                          | AST_MODULO when a1 = Z.zero && a2 = Z.zero -> Modulo(Z.zero, Z.(mod) b1 b2)
+                                          | AST_MODULO when a2 = Z.zero && b2 <= a1 -> Modulo(Z.zero, Z.(mod) b1 b2)
+                                          | AST_MODULO when a2 = Z.zero -> Modulo(Z.gcd a1 (Z.gcd a2 b2), b1) (* avoid division by zero error *)
+                                          | AST_MODULO 
+                                              when a1 = Z.zero 
+                                                && b1 < (Z.max (Z.abs (Z.sub a2 (Z.(mod) b2 a2))) (Z.(mod) b2 a2)) 
+                                              -> Modulo (a1, b1)
+                                          | AST_MODULO -> Modulo(Z.gcd a1 (Z.gcd a2 b2), b1)
+
+    (* comparison *)
+    (* [compare x y op] returns (x',y') where
+        - x' abstracts the set of v  in x such that v op v' is true for some v' in y
+        - y' abstracts the set of v' in y such that v op v' is true for some v  in x
+        i.e., we filter the abstract values x and y knowing that the test is true
+
+        a safe, but not precise implementation, would be:
+        compare x y op = (x,y)
+      *)
+    let rec compare x y op =
+      match x, y with
+      (* congruences performs poorly for comparaison *)
+      (* the only interesting case is the case with two constants *)
+      | Modulo(z, b1), Modulo(z', b2) when z = Z.zero && z' = Z.zero -> begin
         match op with
-        | AST_PLUS -> Modulo (Z.gcd a1 a2, Z.add b1 b2)
-        | AST_MINUS -> Modulo (Z.gcd a1 a2, Z.sub b1 b2)
-        | AST_MULTIPLY ->
-            Modulo
-              ( Z.gcd (Z.mul a1 a2) (Z.gcd (Z.mul a2 b1) (Z.mul a1 b2)),
-                Z.mul b1 b2 )
-        | AST_DIVIDE when a2 = Z.zero ->
-            if b2 = Z.zero then Bot (* division by zero *)
-            else if a1 = Z.zero then Modulo (Z.zero, Z.div b1 b2)
-              (* constant divided by a constant *)
-            else if b2 <= Z.gcd a1 b1 then Modulo (Z.div a1 b2, Z.div a1 b2)
-            else top
-        | AST_DIVIDE -> top
-        | AST_MODULO when (a2, b2) = (Z.zero, Z.zero) ->
-            Bot (* division by zero *)
-        | AST_MODULO when a1 = Z.zero && a2 = Z.zero ->
-            Modulo (Z.zero, Z.( mod ) b1 b2)
-        | AST_MODULO when a2 = Z.zero && b2 <= a1 ->
-            Modulo (Z.zero, Z.( mod ) b1 b2)
-        | AST_MODULO when a2 = Z.zero ->
-            Modulo (Z.gcd a1 (Z.gcd a2 b2), b1)
-            (* avoid division by zero error *)
-        | AST_MODULO
-          when a1 = Z.zero
-               && b1
-                  < Z.max (Z.abs (Z.sub a2 (Z.( mod ) b2 a2))) (Z.( mod ) b2 a2)
-          ->
-            Modulo (a1, b1)
-        | AST_MODULO -> Modulo (Z.gcd a1 (Z.gcd a2 b2), b1))
+        | AST_EQUAL -> if b1 = b2 then x, y else Bot, Bot
+        | AST_NOT_EQUAL -> if b1 != b2 then x, y else Bot, Bot
+        | AST_GREATER -> if b1 > b2 then x, y else Bot, Bot
+        | AST_GREATER_EQUAL -> if b1 >= b2 then x, y else Bot, Bot
+        | AST_LESS -> if b1 < b2 then x, y else Bot, Bot
+        | AST_LESS_EQUAL -> if b1 <= b2 then x, y else Bot, Bot
+        end
+      | _ -> x, y
 
-  (* comparison *)
-  (* [compare x y op] returns (x',y') where
-      - x' abstracts the set of v  in x such that v op v' is true for some v' in y
-      - y' abstracts the set of v' in y such that v op v' is true for some v  in x
-      i.e., we filter the abstract values x and y knowing that the test is true
 
-      a safe, but not precise implementation, would be:
-      compare x y op = (x,y)
-  *)
-  let rec compare x y op =
-    match (x, y) with
-    (* congruences performs poorly for comparaison *)
-    (* the only interesting case is the case with two constants *)
-    | Modulo (z, b1), Modulo (z', b2) when z = Z.zero && z' = Z.zero -> (
-        match op with
-        | AST_EQUAL -> if b1 = b2 then (x, y) else (Bot, Bot)
-        | AST_NOT_EQUAL -> if b1 != b2 then (x, y) else (Bot, Bot)
-        | AST_GREATER -> if b1 > b2 then (x, y) else (Bot, Bot)
-        | AST_GREATER_EQUAL -> if b1 >= b2 then (x, y) else (Bot, Bot)
-        | AST_LESS -> if b1 < b2 then (x, y) else (Bot, Bot)
-        | AST_LESS_EQUAL -> if b1 <= b2 then (x, y) else (Bot, Bot))
-    | _ -> (x, y)
+    (* set-theoretic operations *)
+    let join x y = match x, y with
+                   | _, Bot -> x
+                   | Bot, _ -> y
+                   | Modulo(a1, b1), Modulo(a2, b2) -> Modulo(Z.gcd (Z.gcd a1 a2) (Z.sub b1 b2), b1)
 
-  (* set-theoretic operations *)
-  let join x y =
-    match (x, y) with
-    | _, Bot -> x
-    | Bot, _ -> y
-    | Modulo (a1, b1), Modulo (a2, b2) ->
-        Modulo (Z.gcd (Z.gcd a1 a2) (Z.sub b1 b2), b1)
+    let meet x y = match x, y with
+                   | _, Bot
+                   | Bot, _ -> Bot
+                   | Modulo(a1, b1), Modulo(a2, b2) -> let (g, s, _) = Z.gcdext a1 a2 in
+                                                       if not (Z.congruent b1 b1 g) then Bot
+                                                       else if g = Z.zero then Modulo (Z.zero, b1)
+                                                            else Modulo (
+                                                              Z.lcm a1 a2, 
+                                                              Z.add (Z.mul (Z.sub b2 b1) (Z.mul s (Z.div a1 g))) 
+                                                                     b1)
 
-  let meet x y =
-    match (x, y) with
-    | _, Bot | Bot, _ -> Bot
-    | Modulo (a1, b1), Modulo (a2, b2) ->
-        let g, s, _ = Z.gcdext a1 a2 in
-        if not (Z.congruent b1 b1 g) then Bot
-        else if g = Z.zero then Modulo (Z.zero, b1)
-        else
-          Modulo
-            (Z.lcm a1 a2, Z.add (Z.mul (Z.sub b2 b1) (Z.mul s (Z.div a1 g))) b1)
+    (* backards unary operation *)
+    (* [bwd_unary x op r] return x':
+       - x' abstracts the set of v in x such as op v is in r
+       i.e., we fiter the abstract values x knowing the result r of applying
+       the operation on x
+     *)
+    let bwd_unary x op r = meet x (unary r (ast_uop_inv op))
 
-  (* backards unary operation *)
-  (* [bwd_unary x op r] return x':
-     - x' abstracts the set of v in x such as op v is in r
-     i.e., we fiter the abstract values x knowing the result r of applying
-     the operation on x
-  *)
-  let bwd_unary x op r = meet x (unary r (ast_uop_inv op))
+    (* backward binary operation *)
+    (* [bwd_binary x y op r] returns (x',y') where
+      - x' abstracts the set of v  in x such that v op v' is in r for some v' in y
+      - y' abstracts the set of v' in y such that v op v' is in r for some v  in x
+      i.e., we filter the abstract values x and y knowing that, after
+      applying the operation op, the result is in r
+    *)
+    let bwd_binary x y op r = x, y
 
-  (* backward binary operation *)
-  (* [bwd_binary x y op r] returns (x',y') where
-     - x' abstracts the set of v  in x such that v op v' is in r for some v' in y
-     - y' abstracts the set of v' in y such that v op v' is in r for some v  in x
-     i.e., we filter the abstract values x and y knowing that, after
-     applying the operation op, the result is in r
-  *)
-  let bwd_binary x y op r = (x, y)
 
-  (* widening *)
-  let widen = join
+    (* widening *)
+    let widen = join
 
-  (* narrowing *)
-  let narrow x y = failwith "unimplemented"
+    (* narrowing *)
+    let narrow x y = failwith "unimplemented"
 
-  (* subset inclusion of concretizations *)
-  let subset x y =
-    match (x, y) with
+    (* subset inclusion of concretizations *)
+    let subset x y = match x, y with
     | Bot, _ -> true
     | _, Bot -> false
-    | Modulo (a1, b1), Modulo (a2, b2) -> a1 <= a2 && Z.congruent b1 b2 a2
+    | Modulo(a1, b1), Modulo(a2, b2) -> a1 <= a2 && Z.congruent b1 b2 a2
 
-  (* check the emptiness of the concretization *)
-  let is_bottom x = x = Bot
+    (* check the emptiness of the concretization *)
+    let is_bottom x = x = Bot
 
-  (* print abstract element *)
-  let print fmt x =
-    match x with
-    | Bot -> Format.fprintf fmt "⊥@."
-    | Modulo (a, b) ->
-        Format.fprintf fmt "{ %s·n+%s | n∈ℤ }@." (Z.to_string a) (Z.to_string b)
+    (* print abstract element *)
+    let print fmt x = 
+      match x with
+      | Bot -> Format.fprintf fmt "⊥@."
+      | Modulo(a, b) -> Format.fprintf fmt "{ %s·n+%s | n∈ℤ }@." (Z.to_string a) (Z.to_string b)
 end
--- domains/reduced_product.ml
+++ domains/reduced_product.ml
@@ -1,101 +1,103 @@
 open Frontend
 open Abstract_syntax_tree
 
-module ReducedProductDomain = struct
-  module C = Congruences.CongruencesDomain
-  module I = Interval.IntervalDomain
+module ReducedProductDomain =
+  struct
+    module C = Congruences.CongruencesDomain
+    module I = Interval.IntervalDomain
 
-  (* type of abstract elements *)
-  (* an element of type t abstracts a set of integers *)
-  (* we maintain information on three value domains at the same time *)
-  (* note that the constants domain was not used since its expressivity
-     is included in the intervals domain *)
-  type t = I.t * C.t
+    (* type of abstract elements *)
+    (* an element of type t abstracts a set of integers *)
+    (* we maintain information on three value domains at the same time *)
+    (* note that the constants domain was not used since its expressivity 
+       is included in the intervals domain *)
+    type t = I.t * C.t
 
-  (* unrestricted value: ℤ *)
-  let top = (I.top, C.top)
+    (* unrestricted value: ℤ *)
+    let top = I.top, C.top
 
-  (* bottom value: empty set *)
-  let bottom = (I.bottom, C.bottom)
+    (* bottom value: empty set *)
+    let bottom = I.bottom, C.bottom
 
-  (* constant: {c} *)
-  let const c = (I.const c, C.const c)
+    (* constant: {c} *)
+    let const c = I.const c, C.const c
 
-  (* interval: [a,b] *)
-  let rand a b = (I.rand a b, C.rand a b)
+    (* interval: [a,b] *)
+    let rand a b = I.rand a b, C.rand a b
 
-  (* subset inclusion of concretizations *)
-  let subset (xi, xc) (yi, yc) = I.subset xi yi && C.subset xc yc
+    (* subset inclusion of concretizations *)
+    let subset (xi, xc) (yi, yc) = I.subset xi yi && C.subset xc yc
 
-  (* gathers information from the three domains *)
-  let rec reduction x = x
-  (*let (s, i, c) = match x with
-    (* constants simplification *)
-    | (s, i, C.Modulo(a, b)) when a = Z.zero -> (s, I.meet (I.meet i (I.const b)) (I.from_sign s), C.Modulo (Z.zero, b))
-    (*| (S.Bot, _, _) | (_, I.Bot, _) | (_, _, C.Bot) -> bottom*)
-    | _ -> x
-    in
-    let reduced = (S.meet s (I.to_sign i)), i, C.meet c (I.to_congruences i)
-    in reduced (*if subset x reduced then reduced else reduction reduced*)*)
+    (* gathers information from the three domains *)
+    let rec reduction x = x
+      (*let (s, i, c) = match x with
+      (* constants simplification *)
+      | (s, i, C.Modulo(a, b)) when a = Z.zero -> (s, I.meet (I.meet i (I.const b)) (I.from_sign s), C.Modulo (Z.zero, b))
+      (*| (S.Bot, _, _) | (_, I.Bot, _) | (_, _, C.Bot) -> bottom*)
+      | _ -> x
+      in
+      let reduced = (S.meet s (I.to_sign i)), i, C.meet c (I.to_congruences i)
+      in reduced (*if subset x reduced then reduced else reduction reduced*)*)
 
-  (* unary operation *)
-  let unary (i, c) op = reduction (I.unary i op, C.unary c op)
+    (* unary operation *)
+    let unary (i, c) op = reduction (I.unary i op, C.unary c op)
 
-  (* binary operation *)
-  let binary (xi, xc) (yi, yc) op =
-    reduction (I.binary xi yi op, C.binary xc yc op)
+    (* binary operation *)
+    let binary (xi, xc) (yi, yc) op = reduction (I.binary xi yi op, C.binary xc yc op)
 
-  (* comparison *)
-  (* [compare x y op] returns (x',y') where
-      - x' abstracts the set of v  in x such that v op v' is true for some v' in y
-      - y' abstracts the set of v' in y such that v op v' is true for some v  in x
-      i.e., we filter the abstract values x and y knowing that the test is true
+    (* comparison *)
+    (* [compare x y op] returns (x',y') where
+        - x' abstracts the set of v  in x such that v op v' is true for some v' in y
+        - y' abstracts the set of v' in y such that v op v' is true for some v  in x
+        i.e., we filter the abstract values x and y knowing that the test is true
 
-      a safe, but not precise implementation, would be:
-      compare x y op = (x,y)
-  *)
-  let rec compare (xi, xc) (yi, yc) op =
-    let i1, i2 = I.compare xi yi op and c1, c2 = C.compare xc yc op in
-    (reduction (i1, c1), reduction (i2, c2))
+        a safe, but not precise implementation, would be:
+        compare x y op = (x,y)
+      *)
+    let rec compare (xi, xc) (yi, yc) op =
+      let i1, i2 = I.compare xi yi op
+      and c1, c2 = C.compare xc yc op
+      in reduction ( i1, c1), reduction (i2, c2)
 
-  (* set-theoretic operations *)
-  let join (xi, xc) (yi, yc) = reduction (I.join xi yi, C.join xc yc)
-  let meet (xi, xc) (yi, yc) = reduction (I.meet xi yi, C.meet xc yc)
+    (* set-theoretic operations *)
+    let join (xi, xc) (yi, yc) = reduction (I.join xi yi, C.join xc yc)
 
-  (* backards unary operation *)
-  (* [bwd_unary x op r] return x':
-     - x' abstracts the set of v in x such as op v is in r
-     i.e., we fiter the abstract values x knowing the result r of applying
-     the operation on x
-  *)
-  let bwd_unary (i, c) op (ri, rc) =
-    reduction (I.bwd_unary i op ri, C.bwd_unary c op rc)
+    let meet (xi, xc) (yi, yc) = reduction (I.meet xi yi, C.meet xc yc)
 
-  (* backward binary operation *)
-  (* [bwd_binary x y op r] returns (x',y') where
-     - x' abstracts the set of v  in x such that v op v' is in r for some v' in y
-     - y' abstracts the set of v' in y such that v op v' is in r for some v  in x
-     i.e., we filter the abstract values x and y knowing that, after
-     applying the operation op, the result is in r
-  *)
-  let bwd_binary (xi, xc) (yi, yc) op (ri, rc) =
-    let i1, i2 = I.bwd_binary xi yi op ri
-    and c1, c2 = C.bwd_binary xc yc op rc in
-    (reduction (i1, c1), reduction (i2, c2))
+    (* backards unary operation *)
+    (* [bwd_unary x op r] return x':
+       - x' abstracts the set of v in x such as op v is in r
+       i.e., we fiter the abstract values x knowing the result r of applying
+       the operation on x
+     *)
+    let bwd_unary (i, c) op (ri, rc) = reduction (I.bwd_unary i op ri, C.bwd_unary c op rc)
 
-  (* widening *)
-  let widen (xi, xc) (yi, yc) = reduction (I.widen xi yi, C.widen xc yc)
+    (* backward binary operation *)
+    (* [bwd_binary x y op r] returns (x',y') where
+      - x' abstracts the set of v  in x such that v op v' is in r for some v' in y
+      - y' abstracts the set of v' in y such that v op v' is in r for some v  in x
+      i.e., we filter the abstract values x and y knowing that, after
+      applying the operation op, the result is in r
+    *)
+    let bwd_binary (xi, xc) (yi, yc) op (ri, rc)=
+      let i1, i2 = I.bwd_binary xi yi op ri
+      and c1, c2 = C.bwd_binary xc yc op rc
+      in reduction (i1, c1), reduction (i2, c2)
 
-  (* narrowing *)
-  let narrow (xi, xc) (yi, yc) = reduction (I.narrow xi yi, C.narrow xc yc)
 
-  (* check the emptiness of the concretization *)
-  let is_bottom (i, c) = I.is_bottom i || C.is_bottom c
+    (* widening *)
+    let widen (xi, xc) (yi, yc) = reduction (I.widen xi yi, C.widen xc yc)
 
-  (* print abstract element *)
-  let print fmt (i, c) =
-    Format.fprintf fmt "Product:@.  I: ";
-    I.print fmt i;
-    Format.fprintf fmt "  C: ";
-    C.print fmt c
+    (* narrowing *)
+    let narrow (xi, xc) (yi, yc) = reduction (I.narrow xi yi, C.narrow xc yc)
+
+    (* check the emptiness of the concretization *)
+    let is_bottom (i, c) = I.is_bottom i || C.is_bottom c
+
+    (* print abstract element *)
+    let print fmt (i, c) = 
+      Format.fprintf fmt "Product:@.  I: ";
+      I.print fmt i;
+      Format.fprintf fmt "  C: ";
+      C.print fmt c
 end
